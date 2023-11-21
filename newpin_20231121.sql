b5b40199ab5eb9c808306779aa02ec8eafbb8218
kongtaegyoung <xordud99@gmail.com>
2023-11-21 11:33:22 +0900

    sql 업로드2

--- newpin_20231121.sql
+++ newpin_20231121.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `blog_categories`
 --
 
+
+
 DROP TABLE IF EXISTS `blog_categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
